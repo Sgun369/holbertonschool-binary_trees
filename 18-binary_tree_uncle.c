bb35d8256491b2caa082129580c4759afe045136
Sgun369 <souhaiebjebali@gmail.com>
2023-09-18 16:18:55 +0100

    uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -12,7 +12,6 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
-
 	grandparent = node->parent->parent;
 
 	if (grandparent->left == node->parent)
